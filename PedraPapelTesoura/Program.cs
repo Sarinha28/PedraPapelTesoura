550b55c976dc97f05a2085b59ce5fb51f6150ea9
sara.vcalves <sara.vcalves@ACL062756W10-1>
2023-07-04 15:49:12 -0300

    Adicionar arquivos de projeto.

---
+++ PedraPapelTesoura/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+
+class Program
+{
+    static int rodadas;
+
+    public static void Main(string[] args)
+    {
+        Start();
+    }
+    public static void Start()
+    {
+
+        DesenhaCabecalho(3);
+        Console.WriteLine("Digite \"1\" para começar");
+
+        int iniciar;
+        iniciar = Int32.Parse(Console.ReadLine());
+
+        while (iniciar != 1)
+        {
+
+            DesenhaCabecalho(3);
+            Console.WriteLine("Opção Inválida! Digite Novamente");
+            iniciar = Int32.Parse(Console.ReadLine());
+        }
+
+        DefineRodadas();
+    }
+
+    public static void DesenhaCabecalho(int linhasExtras)
+    {
+        Console.Clear();
+        Console.WriteLine("*******************************");
+        Console.WriteLine("*   Pedra, Papel ou Tesoura   *");
+        Console.WriteLine("*******************************");
+
+        for (int i = 0; i < linhasExtras; i++)
+        {
+            Console.WriteLine("\n");
+        }
+
+    }
+
+    public static void DefineRodadas()
+    {
+
+        DesenhaCabecalho(3);
+        Console.WriteLine("Quantas rodadas você quer jogar? 3, 5 ou 7?");
+        rodadas = Int32.Parse(Console.ReadLine());
+
+        while (rodadas != 3 && rodadas != 5 && rodadas != 7)
+        {
+            DesenhaCabecalho(3);
+
+            Console.WriteLine("Você digitou um valor inválido. Escolha entre os números 3, 5 ou 7:");
+            rodadas = Int32.Parse(Console.ReadLine());
+
+        }
+
+        ControlaRodadas();
+    }
+    public static void ControlaRodadas()
+    {
+        int rodadaAtual = 1;
+        int pontosUsuario = 0;
+        int pontosComputador = 0;
+        bool fimDeJogo = false;
+
+        while (fimDeJogo != true)
+        {
+            DesenhaCabecalho(0);
+            Console.WriteLine("          Rodada " + rodadaAtual.ToString() + "/" + rodadas.ToString() + "        ");
+            Console.WriteLine();
+            Console.WriteLine("User: " + pontosUsuario.ToString() + " pontos  |  CPU: " + pontosComputador.ToString() + " pontos");
+
+            switch (ExibeRodadas())
+            {
+                case 0:
+                    break;
+                case 1:
+                    pontosUsuario++;
+                    rodadaAtual++;
+                    if (pontosUsuario > rodadas / 2)
+                    {
+                        Console.WriteLine("Usuário venceu");
+                    }
+                    break;
+                case 2:
+                    pontosComputador++;
+                    rodadaAtual++;
+                    if (pontosComputador > rodadas / 2)
+                    {
+                        Console.WriteLine("Computador venceu");
+                        fimDeJogo = true;
+                    }
+                    break;
+            }
+            if (fimDeJogo == true)
+            {
+                DesenhaCabecalho(3);
+                if (pontosUsuario > pontosComputador)
+                {
+                    Console.WriteLine("Parabéns, você venceu!!");
+                }
+                else
+                {
+                    Console.WriteLine("Não foi dessa vez!");
+                }
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Digite qualquer tecla para continuar");
+                Console.ReadLine();
+                Start();
+
+            }
+            else
+            {
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Digite 1 para continuar ou 0 para sair");
+                if (Int32.Parse(Console.ReadLine()) == 0)
+                {
+                    Start();
+                }
+            }
+        }
+    }
+    public static int ExibeRodadas()
+    {
+
+        //Algumas variáveis já estão criadas
+        string escolhaDoUsuario; //armazena qual das opções o usuário escolheu
+        string escolhaDoPrograma; //armazena qual da opções o computador sorteou
+        string[] opcoes = {
+      "PEDRA",
+      "PAPEL",
+      "TESOURA"
+    };
+
+        Console.WriteLine("Escolha uma das opções: Pedra, Papel ou Tesoura?");
+        escolhaDoUsuario = Console.ReadLine().ToUpper();
+        while (escolhaDoUsuario != "PEDRA" && escolhaDoUsuario != "PAPEL" && escolhaDoUsuario != "TESOURA")
+        {
+            Console.WriteLine("Você fez uma escolha inválida. Digite novamente: Pedra, Papel ou Tesoura?");
+            escolhaDoUsuario = Console.ReadLine().ToUpper();
+        }
+        //O Computador deve escolher uma das opções e o programa deve exibir qual foi essa escolha
+        Random rand = new Random();
+        int sorteio = rand.Next(opcoes.Length);
+        escolhaDoPrograma = opcoes[sorteio];
+        Console.WriteLine("A escolha do computador foi: " + escolhaDoPrograma);
+        //O programa deve exibir quem ganhou, lembrando que Papel ganha de Pedra, Pedra ganha de Tesoura e Tesoura ganha de Papel
+        if (escolhaDoUsuario == escolhaDoPrograma)
+        {
+            Console.WriteLine("Deu empate");
+            return 0;
+        }
+        else if (escolhaDoUsuario == "PEDRA" && escolhaDoPrograma == "TESOURA")
+        {
+            Console.WriteLine("Parabéns! Você ganhou!");
+            return 1;
+        }
+        else if (escolhaDoUsuario == "TESOURA" && escolhaDoPrograma == "PAPEL")
+        {
+            Console.WriteLine("Parabéns! Você ganhou!");
+            return 1;
+        }
+        else if (escolhaDoUsuario == "PAPEL" && escolhaDoPrograma == "PEDRA")
+        {
+            Console.WriteLine("Parabéns! Você ganhou!");
+            return 1;
+        }
+        else
+        {
+            Console.WriteLine("Que pena! Quem venceu foi o computador!");
+            return 2;
+        }
+    }
+}
+
